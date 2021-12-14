dc7bd98cd021acf5e162abf60d8cda8ccbad430e
thachhn3 <thachhn3@vng.com.vn>
2021-12-14 20:34:01 +0700

    add count song

--- vit/song-service.js
+++ vit/song-service.js
@@ -3,10 +3,14 @@ const Song = require("./song-model");
 const getUniqSongs = async () => {
   const songs = await Song.find();
   const uniqSongs = [];
+
   songs.forEach((song) => {
     const existIndex = uniqSongs.findIndex((s) => s.id === song.id);
     if (existIndex === -1) {
+      song.count = 1;
       uniqSongs.push(song);
+    } else {
+      song.count += 1;
     }
   });
   return uniqSongs;
