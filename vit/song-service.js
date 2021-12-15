d50c5fcbfbe8815cb11338169c9fb884815c4fb0
thachhn3 <thachhn3@vng.com.vn>
2021-12-15 09:07:51 +0700

    update sort song

--- vit/song-service.js
+++ vit/song-service.js
@@ -1,21 +1,21 @@
 const Song = require("./song-model");
 
 const getUniqSongs = async () => {
-  const songs = await Song.find();
+  const songs = await Song.find().lean();
   const uniqSongs = [];
-
   songs.forEach((song) => {
     let existSong = uniqSongs.find((s) => s.id === song.id);
+
     if (!existSong) {
       existSong = song;
       existSong.count = 1;
       uniqSongs.push(existSong);
     } else {
-      existSong.count += 1;
+      existSong.count++;
       songs[uniqSongs.find((s) => s.id === existSong.id)] = existSong;
     }
   });
-  return uniqSongs.sort((a, b) => a.count - b.count);
+  return uniqSongs.sort((a, b) => b.count - a.count);
 };
 
 module.exports = {
